5b32a037f6eedc5634e007365d203b5699cf80ee
nadupalli95 <nadupalli95@gmail.com>
2025-08-13 12:26:57 +0530

    infra creating

---
+++ 70-acm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "lab-remotestate"
+    key            = "expense-acm-dev"
+    region         = "us-east-1"
+    dynamodb_table = "lab-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
